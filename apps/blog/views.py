e6362c25c4d8fbc3ba2750eb9cc2358d2948b3d7
George <113552235+Zhmailo@users.noreply.github.com>
2022-11-25 11:33:10 +0200

    Tag articles

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from apps.blog.models import BlogCategory, Article
+from apps.blog.models import BlogCategory, Article, Tag
 
 
 def blog_category_list(request):
@@ -17,3 +17,14 @@ def article_view(request, category_id, article_id):
     category = BlogCategory.objects.get(id=category_id)
     article = Article.objects.get(id=article_id)
     return render(request, 'blog/article_view.html', {'category': category, 'article': article})
+
+
+def tag_article(request, category_id, article_id, tag_id):
+    tag = Tag.objects.get(id=tag_id)
+    articles = Article.objects.filter(tags__name__icontains=tag.name)
+    return render(request, 'blog/tag_article.html', {'articles': articles, 'tag': tag})
+# Самое читабельное что я нашёл на просторах интернета, ещё можно было смотреть ютуб где я нашёл только один +- норм ролик в котором и так до чёрта рекламы и всё не по сути
+
+
+
+
