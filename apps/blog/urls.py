11a4d9fdaaddc12bf7ba94edc001e88266ed9923
George <113552235+Zhmailo@users.noreply.github.com>
2022-12-09 16:29:57 +0200

    Tag search

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -4,7 +4,7 @@ from apps.blog.views import blog_category_list, article_list, article_view, tag_
 urlpatterns = [
     path('', blog_category_list, name='blog_categories_list'),
     path('<int:category_id>/', article_list, name='blog_article_list'),
-    path('<int:category_id>/<int:article_id>/', article_view, name='blog_article_view'),
+    path('tag/<int:category_id>/<int:article_id>/', article_view, name='blog_article_view'),
     path('<int:category_id>/<int:article_id>/<int:tag_id>/', tag_article, name='blog_tag_article'),
 ]
 
