c783d6eb855dcd4f3f6ab548ffa393fdb6b5fd03
George <113552235+Zhmailo@users.noreply.github.com>
2022-12-23 09:32:07 +0200

    Making admin page look bater

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -4,7 +4,6 @@ from django.urls import reverse
 from django.utils.html import format_html
 from django.utils.http import urlencode
 
-
 admin.site.register(Tag)
 
 
@@ -25,7 +24,7 @@ class BlogCategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'category_link', 'publish_date', 'created_at']
+    list_display = ['id', 'title', 'category_link', 'tag_link', 'created_at']
     list_display_links = ['id', 'title']
     list_filter = ['category', 'tags']
 
@@ -34,3 +33,14 @@ class ArticleAdmin(admin.ModelAdmin):
         return format_html(f"<a href='{url}'>{instance.category.name}</a>")
 
     category_link.short_description = 'Категория'
+
+    def tag_link(self, instance):
+        str_end = f""
+        for tag in instance.tags.all():
+            url = reverse('admin:blog_tag_change', args=[tag.id])
+            str_end += f"<a href='{url}'>#{tag.name}, </a>"
+        str_end = str_end[: str_end.rfind(",")]
+        return format_html(str_end)
+
+    tag_link.short_description = 'Теги'
+
