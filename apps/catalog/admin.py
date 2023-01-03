ee409bcbe8eb2e5ffce26ca5ab35e9a5bfa878c3
George <113552235+Zhmailo@users.noreply.github.com>
2023-01-03 20:18:16 +0200

    Some pr

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.catalog.models import Category, Product
+from apps.catalog.models import Category, Product, ProductImage
 
 
 @admin.register(Category)
@@ -12,7 +12,16 @@ class ProductCategoryInline(admin.TabularInline):
     extra = 1
 
 
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    fields = ['product', 'image_tag', 'image', 'is_main']
+    readonly_fields = ['image_tag']
+    extra = 1
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['name']}
-    inlines = [ProductCategoryInline]
+    inlines = [ProductCategoryInline, ProductImageInline]
+    list_display = ['id', 'image_tag', 'name', 'quantity', 'price', 'created_at']
+    list_display_links = ['id', 'image_tag', 'name']
--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.safestring import mark_safe
-from imagekit.models import ProcessedImageField
+from imagekit.models import ProcessedImageField, ImageSpecField
 from mptt.models import MPTTModel, TreeForeignKey
 from django.urls import reverse
 from pilkit.processors import ResizeToFill
@@ -56,6 +56,45 @@ class Category(MPTTModel):
         verbose_name_plural = 'Категории'
 
 
+class ProductImage(models.Model):
+    image = ProcessedImageField(
+        verbose_name='Изображение',
+        upload_to='catalog/product/',
+    )
+    image_thumbnail = ImageSpecField(
+        source='image',
+        processors=[ResizeToFill(600, 400)]
+    )
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    is_main = models.BooleanField(verbose_name='Основное изображение', default=False)
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if self.is_main:
+            ProductImage.objects.filter(product=self.product).update(is_main=False)
+        super().save(force_insert, force_update, using, update_fields)
+
+    def __str__(self):
+        return ''
+
+    def image_tag_thumbnail(self):
+        if not self.image_thumbnail:
+            ProductImage.objects.get(id=self.id)
+        return mark_safe(f"<img src='/{MEDIA_ROOT}{self.image_thumbnail}' width='70'>")
+
+    image_tag_thumbnail.short_description = 'Текущее изображение'
+
+    def image_tag(self):
+        if not self.image_thumbnail:
+            ProductImage.objects.get(id=self.id)
+        return mark_safe(f"<img src='/{MEDIA_ROOT}{self.image_thumbnail}'>")
+
+    image_tag.short_description = 'Текущее изображение'
+
+    class Meta:
+        verbose_name = 'Изображение товара'
+        verbose_name_plural = 'Изображения товара'
+
+
 class Product(models.Model):
     name = models.CharField(verbose_name='Название', max_length=255)
     slug = models.SlugField(unique=True, verbose_name='Слаг (ЧПУ)')
@@ -66,8 +105,25 @@ class Product(models.Model):
     updated_at = models.DateTimeField(verbose_name='Дата изменения', auto_now=True)
     created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
 
+    def images(self):
+        return ProductImage.objects.filter(product=self.id)
+
+    def main_image(self):
+        image = ProductImage.objects.filter(product=self.id, is_main=True).first()
+        if not image:
+            image = self.images().first()
+        return image
+
+    def image_tag(self):
+        image = self.main_image()
+        if image:
+            return image.image_tag_thumbnail()
+
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('product', args=[self.slug])
 
     class Meta:
         verbose_name = 'Товар'
@@ -83,6 +139,9 @@ class ProductCategory(models.Model):
         if self.is_main:
             ProductCategory.objects.filter(product=self.product).update(is_main=False)
         super().save(force_insert, force_update, using, update_fields)
+
+    def __str__(self):
+        return ''
 
     class Meta:
         verbose_name = 'Категория товара'
