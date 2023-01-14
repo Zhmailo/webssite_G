370a049aa8443aca2de2d51384a85cca5fb0360b
George <113552235+Zhmailo@users.noreply.github.com>
2023-01-14 16:48:52 +0200

    breadcrumbs

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -5,7 +5,12 @@ from apps.blog.models import BlogCategory, Article, Tag
 
 def blog_category_list(request):
     blog_categories = BlogCategory.objects.all()
-    return render(request, 'blog/category_list.html', {'categories': blog_categories})
+    breadcrumbs = {'current': 'Блог'}
+
+    return render(
+        request,
+        'blog/category_list.html',
+        {"categories": blog_categories, 'breadcrumbs': breadcrumbs})
 
 
 def article_list(request, category_id):
@@ -18,10 +23,24 @@ def article_list(request, category_id):
 def article_view(request, category_id, article_id):
     category = BlogCategory.objects.get(id=category_id)
     article = Article.objects.get(id=article_id)
-    return render(request, 'blog/article_view.html', {'category': category, 'article': article})
+    breadcrumbs = {
+        reverse('blog_category_list'): 'Блог',
+        reverse('blog_article_list', args=[category_id]): category.name,
+        'current': article.title
+    }
+    return render(
+        request,
+        'blog/article_view.html',
+        {'article': article, 'category': category, 'breadcrumbs': breadcrumbs})
 
 
 def tag_search_view(request, tag_id):
     tag = Tag.objects.get(id=tag_id)
     articles = Article.objects.filter(tags=tag)
-    return render(request, 'blog/tag_search.html', {'tag': tag, 'articles': articles})
+    breadcrumbs = {
+        reverse('blog_category_list'): 'Блог',
+        'current': tag.name
+    }
+    return render(request,
+                  'blog/tag_article.html',
+                  {'tag': tag, 'articles': articles, 'breadcrumbs': breadcrumbs})
