2830fd1121c81821b7c5cae3700bc54b337ccb04
George <113552235+Zhmailo@users.noreply.github.com>
2023-01-17 18:10:49 +0200

    registration

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 
-from apps.user.forms import LoginForm
+from apps.user.forms import LoginForm, RegisterForm
 
 
 def user_login(request):
@@ -11,8 +11,28 @@ def user_login(request):
         if form.is_valid():
             cleaned_data = form.cleaned_data
             user = authenticate(username=cleaned_data['username'], password=cleaned_data['password'])
+            print(user)
+            print(cleaned_data)
             if user:
                 login(request, user)
                 return redirect('index')
         error = 'Направельний логін або пароль'
     return render(request, 'user/login.html', {'error': error})
+
+
+def user_register(request):
+    error = None
+    if request.method == 'POST':
+        form = RegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return render(request, 'user/welcome.html', {'user': user})
+        error = form.errors
+    return render(request, 'user/register.html', {'error': error})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('index')
